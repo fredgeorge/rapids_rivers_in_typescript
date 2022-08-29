91d1259d9e7bfa6ae1b5f836717a5cb96e2cbe7c
Fred George <fredgeorge@acm.org>
2022-08-29 11:39:07 +0200

    Fred: Packet renders itself into JSON string appropriately

---
+++ src/test/unit/packet_render.test.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 by Fred George
+ * @author Fred George  fredgeorge@acm.org
+ * Licensed under the MIT License; see LICENSE file in root.
+ */
+
+import Packet from "../../main/packets/packet";
+
+
+test('render unchanged without missing keys', () => {
+    let packet = new Packet(`
+        {
+            "integer_key": 7,
+            "boolean_key": true,
+            "null_key":null,
+            "empty_string":"",
+            "empty_list_key":[]
+        }
+    `)
+    expect(packet.toJsonString()).toBe('{"integer_key":7,"boolean_key":true}')
+})
+
+test('render additional keys', () => {
+    let packet = new Packet(`
+        {
+            "integer_key": 7,
+            "boolean_key": true
+        }
+    `)
+    packet.new_key = "new value"
+    expect(packet.toJsonString()).toBe('{"integer_key":7,"boolean_key":true,"new_key":"new value"}')
+})
+
+test('render changed without missing keys', () => {
+    let packet = new Packet(`
+        {
+            "integer_key": 7,
+            "boolean_key": true
+        }
+    `)
+    packet.integer_key = 9
+    expect(packet.toJsonString()).toBe('{"integer_key":9,"boolean_key":true}')
+    packet.boolean_key = false
+    expect(packet.toJsonString()).toBe('{"integer_key":9,"boolean_key":false}')
+})
